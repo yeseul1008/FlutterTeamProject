ab78f19c1a4dd778dc3a3aedb583b8fd56285d09
KIMYESEUL <kimesil1008@ajou.ac.kr>
2025-12-26 16:03:40 +0900

    페이지작업

--- lib/routePage1.dart
+++ lib/routePage1.dart
@@ -1,33 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
-class StartPage extends StatelessWidget {
-  const StartPage({super.key});
+class Page1 extends StatelessWidget {
+  // case2 : /page1 주소로 이동 시 실행 페이지
+  const Page1({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("개발 중 시작 페이지 입니다."),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text(
-              '이동 필요한 부분들 넣으세요',
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: () {
-                context.go('/userLogin');
-              },
-              child: const Text('로그인 페이지로 이동'),
-            ),
-          ],
-        ),
-      ),
+      appBar: AppBar(title : Text("page1")),
+      body : Center(
+        child: Text("page1"),
+      )
     );
   }
 }
